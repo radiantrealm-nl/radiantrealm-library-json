d9773a3cc5fc0ea885c1a5a06b2d8f194dddd306
Josje <paul.verbael@gmail.com>
2025-10-21 00:36:07 +0200

    Add custom JsonException class
    Introduces a JsonException class extending Exception to provide custom exception handling for JSON-related errors.

---
+++ src/main/java/nl/radiantrealm/json/JsonException.java
@@ -0,0 +1,12 @@
+package nl.radiantrealm.json;
+
+public class JsonException extends Exception {
+
+    public JsonException() {
+        super();
+    }
+
+    public JsonException(String message) {
+        super(message);
+    }
+}
